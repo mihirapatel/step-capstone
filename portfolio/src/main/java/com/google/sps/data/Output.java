854309e062063ba44a9113e7820a0e3ddb6e8244
Angela Liu <aliu917@berkeley.edu>
2020-06-18 15:11:27 -0700

    middle of merging

---
+++ portfolio/src/main/java/com/google/sps/data/Output.java
@@ -0,0 +1,35 @@
+package com.google.sps.data;
+
+/** Ouptut shown on web app screen. */
+public final class Output {
+
+  private final String userInput;
+  private final String fulfillmenttext;
+  private final String display;
+
+  // Constructor without display
+  public Output(String userInput, String fulfillmentText) {
+    this.userInput = userInput;
+    this.fulfillmentText = fulfillmentText;
+    this.display = null;
+  }
+
+  // Constructor with display
+  public Output(String userInput, String fulfillmentText, String display) {
+    this.userInput = userInput;
+    this.fulfillmentText = fulfillmentText;
+    this.display = display;
+  }
+
+  public String getUserInput() {
+    return this.userInput;
+  }
+
+  public String getFulfillmentText() {
+    return this.fulfillmentText;
+  }
+
+  public String getDisplay() {
+    return this.display;
+  }
+}
