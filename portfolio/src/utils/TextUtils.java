ca2f3cc38a16e2f800763ef348621f9516f687a9
Angela Liu <aliu917@berkeley.edu>
2020-06-18 09:43:30 -0700

    Abby's stuff

---
+++ portfolio/src/utils/TextUtils.java
@@ -0,0 +1,48 @@
+package com.google.sps.utils;
+
+import com.google.cloud.dialogflow.v2.QueryInput;
+import com.google.cloud.dialogflow.v2.QueryResult;
+import com.google.cloud.dialogflow.v2.SessionName;
+import com.google.cloud.dialogflow.v2.SessionsClient;
+import java.io.IOException;
+import com.google.cloud.dialogflow.v2.TextInput;
+import com.google.cloud.dialogflow.v2.DetectIntentResponse;
+
+/**
+ * DialogFlow API Detects Intent with input text.
+**/
+
+public class TextUtils {
+  public static QueryResult detectIntentStream(String text) {
+    QueryResult queryResult = null;
+
+    try (SessionsClient sessionsClient = SessionsClient.create()) {
+      // Set the session name using the sessionId (UUID) and projectID (my-project-id)
+      SessionName session = SessionName.of("fair-syntax-280601", "1");
+        String languageCode = "en-US";
+        
+        // Set the text and language code (en-US) for the query
+        TextInput.Builder textInput =
+            TextInput.newBuilder().setText(text).setLanguageCode(languageCode);
+
+        // Build the query with the TextInput
+        QueryInput queryInput = QueryInput.newBuilder().setText(textInput).build();
+
+        // Performs the detect intent request
+        DetectIntentResponse response = sessionsClient.detectIntent(session, queryInput);
+
+        // Display the query result
+        queryResult = response.getQueryResult();
+
+        System.out.println("====================");
+        System.out.format("Query Text: '%s'\n", queryResult.getQueryText());
+        System.out.format("Detected Intent: %s (confidence: %f)\n",
+            queryResult.getIntent().getDisplayName(), queryResult.getIntentDetectionConfidence());
+        System.out.format("Fulfillment Text: '%s'\n", queryResult.getFulfillmentText());
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return queryResult;
+  }
+}
