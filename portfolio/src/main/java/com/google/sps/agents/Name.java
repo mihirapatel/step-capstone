f8929c75c577c9ec11ef47586de31ce0aaa1c3a3
Angela Liu <aliu917@berkeley.edu>
2020-06-23 16:39:03 -0700

    Added nickname feature.

--- portfolio/src/main/java/com/google/sps/agents/Name.java
+++ portfolio/src/main/java/com/google/sps/agents/Name.java
@@ -7,6 +7,7 @@ import com.google.protobuf.Struct;
 import com.google.protobuf.Value;
 import com.google.sps.data.Output;
 import com.google.sps.agents.Agent;
+import com.google.sps.utils.UserUtils;
 import java.io.IOException;
 import java.util.Map;
  
@@ -15,20 +16,47 @@ import java.util.Map;
  */
 public class Name implements Agent {
     private final String intentName;
-  	private String searchText;
+    String outputText;
+    String userID;
     
     public Name(String intentName, Map<String, Value> parameters) {
-        this.intentName = intentName;
+      this.intentName = intentName;
+      userID = UserUtils.getUserID();
+      if (userID == null) {
+        outputText = "Please login to modify your name.";
+      } else {
         setParameters(parameters);
+      }
     }
 
 	@Override 
 	public void setParameters(Map<String, Value> parameters) {
+      System.out.println("Name parameters");
+      System.out.println(parameters);
+      String nameType = parameters.get("type").getStringValue();
+      String name = null;
+      nameType = nameType.equals("") ? "first name" : nameType;
+      switch(nameType) {
+        case "first name":
+          name = parameters.get("given-name").getStringValue();
+          break;
+        case "middle name":
+          name = parameters.get("given-name").getStringValue();
+          break;
+        case "last name":
+          name = parameters.get("last-name").getStringValue();
+          break;
+        case "nickname":
+          name = parameters.get("nick-name").getStringValue();
+          name = (name == null) ? parameters.get("given-name").getStringValue() : name;
+      }
+      UserUtils.saveName(nameType, name);
+      outputText = "Changing your " + nameType + " to be " + name;
 	}
 	
 	@Override
 	public String getOutput() {
-	    return null;
+	  return outputText;
 	}
 
 	@Override
