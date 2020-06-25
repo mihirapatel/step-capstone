eb16d7a4618ae5cc158923079a8f697c1aff536a
aliu917 <46185406+aliu917@users.noreply.github.com>
2020-06-25 13:51:31 -0700

    Update Reminders.java
    Reformat spacing to tab-alignment

--- portfolio/src/main/java/com/google/sps/agents/Reminders.java
+++ portfolio/src/main/java/com/google/sps/agents/Reminders.java
@@ -17,45 +17,45 @@ import java.util.Map;
  */
 public class Reminders implements Agent {
     private String intentName = null;
-  	private String recurrence = null;
+    private String recurrence = null;
     private Date date = null;
     private String name = "";
     private String fulfillment = "";
     private String display = null;
     
     public Reminders(String intentName, Map<String, Value> parameters) {
-      this.intentName = intentName;
-      try {
-        setParameters(parameters);
-      } catch (Exception e) {
-        fulfillment = null;
-      }
+        this.intentName = intentName;
+        try {
+            setParameters(parameters);
+        } catch (Exception e) {
+            fulfillment = null;
+        }
     }
 
-	@Override 
-	public void setParameters(Map<String, Value> parameters) {
-      if (intentName.equals("snooze")) {
-        Struct durationStruct = parameters.get("date-time").getStructValue();
-        Map<String, Value> durationMap = durationStruct.getFieldsMap();
-        Date start = TimeUtils.stringToDate(durationMap.get("startDateTime").getStringValue());
-        Date end = TimeUtils.stringToDate(durationMap.get("endDateTime").getStringValue());
-        int diffSec = (int)((end.getTime() - start.getTime()) / 1000);
-        String timeDisplay = TimeUtils.timeToString(diffSec);
-        if (timeDisplay == null) {
-          fulfillment = "Sorry, unable to set a timer for less than 1 second or more than 1 day. Please try adding a reminder instead.";
-        } else {
-          fulfillment = "Starting a timer for " + timeDisplay + " now.";
-          display = TimeUtils.makeClockDisplay(diffSec);
-        }
-      } else {
-        fulfillment = "Sorry, the reminders feature is not supported yet.";
-        //TODO: if we ever get access to storing user info or calendars...
-      }
+    @Override 
+    public void setParameters(Map<String, Value> parameters) {
+        if (intentName.equals("snooze")) {
+			Struct durationStruct = parameters.get("date-time").getStructValue();
+			Map<String, Value> durationMap = durationStruct.getFieldsMap();
+			Date start = TimeUtils.stringToDate(durationMap.get("startDateTime").getStringValue());
+			Date end = TimeUtils.stringToDate(durationMap.get("endDateTime").getStringValue());
+			int diffSec = (int)((end.getTime() - start.getTime()) / 1000);
+			String timeDisplay = TimeUtils.timeToString(diffSec);
+			if (timeDisplay == null) {
+				fulfillment = "Sorry, unable to set a timer for less than 1 second or more than 1 day. Please try adding a reminder instead.";
+			} else {
+				fulfillment = "Starting a timer for " + timeDisplay + " now.";
+				display = TimeUtils.makeClockDisplay(diffSec);
+			}
+		 } else {
+			fulfillment = "Sorry, the reminders feature is not supported yet.";
+			//TODO: if we ever get access to storing user info or calendars...
+		 }
 	}
 	
 	@Override
 	public String getOutput() {
-	  return fulfillment;
+	    return fulfillment;
 	}
 
 	@Override
