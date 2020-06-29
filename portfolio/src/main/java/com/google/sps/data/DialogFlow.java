d5e6478bb70329fc24d9a2e4df3299aac125bf5f
Angela Liu <aliu917@berkeley.edu>
2020-06-29 15:17:51 -0700

    Created DialogFlow class separate

--- portfolio/src/main/java/com/google/sps/data/DialogFlow.java
+++ portfolio/src/main/java/com/google/sps/data/DialogFlow.java
@@ -6,27 +6,27 @@ import com.google.cloud.dialogflow.v2.QueryResult;
 import com.google.cloud.dialogflow.v2.SessionName;
 import com.google.cloud.dialogflow.v2.SessionsClient;
 import com.google.cloud.dialogflow.v2.TextInput;
-import java.io.IOException;
 
 public class DialogFlow {
 
-    static SessionName session = SessionName.of("mihira-step-2020-3", "1");
-    QueryResult queryResult;
+  static SessionName session = SessionName.of("mihira-step-2020-3", "1");
+  QueryResult queryResult;
 
-    public DialogFlow(String text, String languageCode, SessionsClient sessionsClient) {
-            TextInput.Builder textInput = TextInput.newBuilder().setText(text).setLanguageCode(languageCode);
+  public DialogFlow(String text, String languageCode, SessionsClient sessionsClient) {
+    TextInput.Builder textInput =
+        TextInput.newBuilder().setText(text).setLanguageCode(languageCode);
 
-            // Build the query with the TextInput
-            QueryInput queryInput = QueryInput.newBuilder().setText(textInput).build();
+    // Build the query with the TextInput
+    QueryInput queryInput = QueryInput.newBuilder().setText(textInput).build();
 
-            // Performs the detect intent request
-            DetectIntentResponse response = sessionsClient.detectIntent(session, queryInput);
+    // Performs the detect intent request
+    DetectIntentResponse response = sessionsClient.detectIntent(session, queryInput);
 
-            // Display the query result
-            queryResult = response.getQueryResult();
-    }
+    // Display the query result
+    queryResult = response.getQueryResult();
+  }
 
-    public QueryResult getResponse() {
-        return queryResult;
-    }
+  public QueryResult getResponse() {
+    return queryResult;
+  }
 }
--- portfolio/src/main/java/com/google/sps/utils/TextUtils.java
+++ portfolio/src/main/java/com/google/sps/utils/TextUtils.java
@@ -5,29 +5,26 @@ import com.google.cloud.dialogflow.v2.SessionsClient;
 import com.google.sps.data.DialogFlow;
 import java.io.IOException;
 
-/**
- * DialogFlow API Detects Intent with input text.
-**/
-
+/** DialogFlow API Detects Intent with input text. */
 public class TextUtils {
 
+  public static QueryResult detectIntentStream(String text, String languageCode) {
+    QueryResult queryResult = null;
 
-    public static QueryResult detectIntentStream(String text, String languageCode) {
-        QueryResult queryResult = null;
+    try (SessionsClient sessionsClient = SessionsClient.create()) {
+      DialogFlow dialogFlow = new DialogFlow(text, languageCode, sessionsClient);
+      queryResult = dialogFlow.getResponse();
 
-        try (SessionsClient sessionsClient = SessionsClient.create()) {
-            DialogFlow dialogFlow = new DialogFlow(text, languageCode, sessionsClient);
-            queryResult = dialogFlow.getResponse();
+      System.out.println("====================");
+      System.out.format("Query Text: '%s'\n", queryResult.getQueryText());
+      System.out.format(
+          "Detected Intent: %s (confidence: %f)\n",
+          queryResult.getIntent().getDisplayName(), queryResult.getIntentDetectionConfidence());
+      System.out.format("Fulfillment Text: '%s'\n", queryResult.getFulfillmentText());
 
-            System.out.println("====================");
-            System.out.format("Query Text: '%s'\n", queryResult.getQueryText());
-            System.out.format("Detected Intent: %s (confidence: %f)\n",
-                queryResult.getIntent().getDisplayName(), queryResult.getIntentDetectionConfidence());
-            System.out.format("Fulfillment Text: '%s'\n", queryResult.getFulfillmentText());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return queryResult;
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+    return queryResult;
+  }
 }
