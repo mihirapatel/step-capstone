a8da3f8daf6248dc55a08cbabb2e1db347f12aa4
Mihira Patel <mihirapatel@berkeley.edu>
2020-06-30 11:10:42 -0700

    Update Currency.java

--- portfolio/src/main/java/com/google/sps/agents/Currency.java
+++ portfolio/src/main/java/com/google/sps/agents/Currency.java
@@ -13,44 +13,45 @@ import java.util.Map;
 /**
  * Currency Agent
  */
+
 public class Currency implements Agent {
-	private final String intentName;
-	private String currencyFrom;
-	private String currencyTo;
-	private Double amount;
-	private String fulfillment = null;;
-	private String diplay = null;
-	private String redirect = null;
+  private final String intentName;
+  private String currencyFrom;
+  private String currencyTo;
+  private Double amount;
+  private String fulfillment = null;;
+  private String diplay = null;
+  private String redirect = null;
 
-	public Currency(String intentName, Map<String, Value> parameters) {
-		this.intentName = intentName;
-		setParameters(parameters);
-	}
+  public Currency(String intentName, Map<String, Value> parameters) {
+	  this.intentName = intentName;
+	  setParameters(parameters);
+  }
 
-	@Override 
-	public void setParameters(Map<String, Value> parameters) {
-		currencyFrom = parameters.get("currency-from").getStringValue();
-		currencyTo = parameters.get("currency-to").getStringValue();
-		amount = parameters.get("amount").getNumberValue();
+  @Override 
+  public void setParameters(Map<String, Value> parameters) {
+	  currencyFrom = parameters.get("currency-from").getStringValue();
+	  currencyTo = parameters.get("currency-to").getStringValue();
+	  amount = parameters.get("amount").getNumberValue();
 
-		fulfillment = "Redirecting for conversion";
-		String baseURL = "http://www.google.com/search?q=";
-		String endURL = String.join("+", "Convert", String.valueOf(amount), currencyFrom, "to", currencyTo); 
-		redirect = baseURL + endURL;
-	}
+	  fulfillment = "Redirecting for conversion";
+	  String baseURL = "http://www.google.com/search?q=";
+	  String endURL = String.join("+", "Convert", String.valueOf(amount), currencyFrom, "to", currencyTo); 
+	  redirect = baseURL + endURL;
+  }
 
-	@Override
-	public String getOutput() {
-		return fulfillment;
-	}
+  @Override
+  public String getOutput() {
+	  return fulfillment;
+  }
 
-	@Override
-	public String getDisplay() {
-		return display;
-	}
+  @Override
+  public String getDisplay() {
+	  return display;
+  }
 
-	@Override
-	public String getRedirect() {
-		return redirect;
-	}
+  @Override
+  public String getRedirect() {
+	  return redirect;
+  }
 }
