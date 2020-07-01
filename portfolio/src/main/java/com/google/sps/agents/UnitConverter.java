81104b400231952d9efd6538fb0a4ff45bbfe9bc
Mihira <mihirapatel@berkeley.edu>
2020-07-01 21:57:12 +0000

    Fixed unit converter agent to account for not having all params and added tests for it

--- portfolio/src/main/java/com/google/sps/agents/UnitConverter.java
+++ portfolio/src/main/java/com/google/sps/agents/UnitConverter.java
@@ -1,55 +1,71 @@
 package com.google.sps.agents;
- 
+
 // Imports the Google Cloud client library
-import com.google.cloud.dialogflow.v2.QueryInput;
-import com.google.cloud.dialogflow.v2.QueryResult;
-import com.google.protobuf.Struct;
 import com.google.protobuf.Value;
-import com.google.sps.data.Output;
-import com.google.sps.agents.Agent;
-import java.io.IOException;
 import java.util.Map;
- 
-/**
- * Unit Converter Agent
- */
+
+/** Unit Converter Agent */
 public class UnitConverter implements Agent {
-    private final String intentName;
-  	private String unitFrom;
-    private String unitTo;
-    private Double amount;
-    private String fulfillment;
-    private String redirect;
-  
-    public UnitConverter(String intentName, Map<String, Value> parameters) {
-        this.intentName = intentName;
-        setParameters(parameters);
+  private final String intentName;
+  private String unitFrom;
+  private String unitTo;
+  private Double amount;
+  private String fulfillment = null;
+  private String display = null;
+  private String redirect = null;
+  private String searchText = "";
+  private String baseURL;
+  private String endURL;
+
+  public UnitConverter(String intentName, Map<String, Value> parameters) {
+    this.intentName = intentName;
+    setParameters(parameters);
+  }
+
+  @Override
+  public void setParameters(Map<String, Value> parameters) {
+    unitFrom = parameters.get("unit-from").getStringValue();
+    unitTo = parameters.get("unit-to").getStringValue();
+    amount = parameters.get("amount").getNumberValue();
+    fulfillment = "Redirecting for conversion";
+    baseURL = "http://www.google.com/search?q=";
+
+    searchText += "Convert";
+    if (amount > 0.0) {
+      searchText += " " + String.valueOf(amount);
     }
-
-	@Override 
-	public void setParameters(Map<String, Value> parameters) {
-        unitFrom = parameters.get("unit-from").getStringValue();
-        unitTo = parameters.get("unit-to").getStringValue();
-        amount = parameters.get("amount").getNumberValue();
-
-        fulfillment = "Redirecting for conversion";
-        String baseURL = "http://www.google.com/search?q=";
-        String endURL = String.join("+", "Convert", String.valueOf(amount), unitFrom, "to", unitTo);
-		redirect = baseURL + endURL;
-	}
-	
-	@Override
-	public String getOutput() {
-	    return "Redirecting for conversion";
-	}
-
-	@Override
-	public String getDisplay() {
-		return null;
-	}
-
-	@Override
-	public String getRedirect() {
-        return redirect;
+    if (!unitFrom.equals("")) {
+      searchText += " " + unitFrom;
     }
+    if (!unitTo.equals("")) {
+      if (!unitFrom.equals("")) {
+        searchText += " to";
+      }
+      searchText += " " + unitTo;
+    }
+    String[] individualWords = searchText.split(" ");
+    endURL = String.join("+", individualWords);
+    redirect = baseURL + endURL;
+
+    System.out.println("unitFrom: " + unitFrom);
+    System.out.println("unitTo: " + unitTo);
+    System.out.println("amount: " + amount);
+    System.out.println("fulfillment: " + fulfillment);
+    System.out.println("redirect: " + redirect);
+  }
+
+  @Override
+  public String getOutput() {
+    return fulfillment;
+  }
+
+  @Override
+  public String getDisplay() {
+    return display;
+  }
+
+  @Override
+  public String getRedirect() {
+    return redirect;
+  }
 }
