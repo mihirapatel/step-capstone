3e881962f67b6f01cfb14743608ca5e6e0b1b647
Abby Mapes <acm103@duke.edu>
2020-07-09 18:58:52 +0000

    Add memory store and delete

--- portfolio/src/main/java/com/google/sps/agents/Books.java
+++ portfolio/src/main/java/com/google/sps/agents/Books.java
@@ -1,6 +1,13 @@
 package com.google.sps.agents;
 
 // Imports the Google Cloud client library
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.Text;
 import com.google.protobuf.Value;
 import com.google.sps.data.Book;
 import com.google.sps.data.BookQuery;
@@ -40,22 +47,33 @@ public class Books implements Agent {
       ArrayList<Book> results = BookUtils.getRequestedBooks(query, startIndex);
       this.totalResults = BookUtils.getTotalVolumesFound(query, startIndex);
       this.resultsReturned = results.size();
-      System.out.println(totalResults);
-      System.out.println(resultsReturned);
 
-      // Make output information
+      if (resultsReturned > 0) {
+        // Delete stored BookQuery, Book results, totalResults, resultsReturned
+        deleteAllStoredBookInformation();
+        // Store BookQuery, Book results, totalResults, resultsReturned
+        storeBooks(results);
+        storeBookQuery(query);
+        storeIndices();
 
-      // Delete stored BookQuery, Book results, totalResults, resultsReturned
-      // Store BookQuery, Book results, totalResults, resultsReturned
+        // Get output information from stored Book results
+
+        // Set fulfillment
+        this.output = "Here's what I found.";
+      } else {
+        this.output = "I couldn't find any results. Can you try again?";
+      }
+
     } else if (intentName.equals("more")) {
       // Load BookQuery, totalResults, resultsReturned
-
       // Increment startIndex
       // Retrieve books
-      // Make output information
 
       // Delete stored BookQuery, Book results, totalResults, resultsReturned
       // Store BookQuery, Book results, totalResults, resultsReturned
+
+      // Get output information from stored Book results
+
     } else if (intentName.equals("about")) {
       // Load Book results, totalResults, resultsReturned
 
@@ -73,7 +91,7 @@ public class Books implements Agent {
     } else if (intentName.equals("results")) {
       // Load Book results, totalResults, resultsReturned
 
-      // Set display and fulfillment for original table
+      // Get output information from stored Book results
 
       // Don't change any stored information
     }
@@ -86,7 +104,6 @@ public class Books implements Agent {
 
   @Override
   public String getDisplay() {
-    this.display = "hello";
     return this.display;
   }
 
@@ -94,4 +111,97 @@ public class Books implements Agent {
   public String getRedirect() {
     return this.redirect;
   }
+
+  private void storeBooks(ArrayList<Book> books) {
+    for (int i = 0; i < books.size(); ++i) {
+      long timestamp = System.currentTimeMillis();
+      Book currentBook = books.get(i);
+      Entity bookEntity = new Entity("Book");
+      bookEntity.setProperty("title", currentBook.getTitle());
+      bookEntity.setProperty("publishedDate", currentBook.getPublishedDate());
+      bookEntity.setProperty("authors", currentBook.getAuthors());
+      bookEntity.setProperty("averageRating", currentBook.getRating());
+      bookEntity.setProperty("infoLink", currentBook.getInfoLink());
+      bookEntity.setProperty("thumbnailLink", currentBook.getThumbnailLink());
+      bookEntity.setProperty("buyLink", currentBook.getBuyLink());
+      bookEntity.setProperty("embeddable", currentBook.getEmbeddable());
+      bookEntity.setProperty("isbn", currentBook.getISBN());
+      bookEntity.setProperty("timestamp", timestamp);
+
+      Text textDescription = new Text(currentBook.getDescription());
+      Text textSnippet = new Text(currentBook.getTextSnippet());
+      bookEntity.setProperty("description", textDescription);
+      bookEntity.setProperty("textSnippet", textSnippet);
+
+      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+      datastore.put(bookEntity);
+    }
+  }
+
+  private void storeBookQuery(BookQuery query) {
+    long timestamp = System.currentTimeMillis();
+    Entity bookQueryEntity = new Entity("BookQuery");
+    bookQueryEntity.setProperty("userInput", query.getUserInput());
+    bookQueryEntity.setProperty("type", query.getType());
+    bookQueryEntity.setProperty("categories", query.getCategories());
+    bookQueryEntity.setProperty("authors", query.getAuthors());
+    bookQueryEntity.setProperty("title", query.getTitle());
+    bookQueryEntity.setProperty("order", query.getOrder());
+    bookQueryEntity.setProperty("language", query.getLanguage());
+    bookQueryEntity.setProperty("queryString", query.getQueryString());
+    bookQueryEntity.setProperty("timestamp", timestamp);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(bookQueryEntity);
+  }
+
+  private void storeIndices() {
+    long timestamp = System.currentTimeMillis();
+    Entity indicesEntity = new Entity("Indices");
+    indicesEntity.setProperty("startIndex", this.startIndex);
+    indicesEntity.setProperty("resultsReturned", this.resultsReturned);
+    indicesEntity.setProperty("totalResults", this.totalResults);
+    indicesEntity.setProperty("timestamp", timestamp);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(indicesEntity);
+  }
+
+  private void deleteAllStoredBookInformation() {
+    deleteFromDatastore("BookQuery");
+    deleteFromDatastore("Book");
+    deleteFromDatastore("Indices");
+  }
+
+  private void deleteFromDatastore(String entityName) {
+    Query query = new Query(entityName).addSort("timestamp", SortDirection.DESCENDING);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+    for (Entity entity : results.asIterable()) {
+      datastore.delete(entity.getKey());
+    }
+  }
+
+  /*private String getDisplayTableFromArrayList(ArrayList<Book> books, int numToDisplay) {
+    for (int i = 0; i < numToDisplay; ++i) {
+        Book book = getB
+        if (all == false && i >= userQuantity){
+            break;
+        }
+        else{
+            String userComment = (String) entity.getProperty("comment");
+            String name = (String) entity.getProperty("name");
+            String userEmail = (String) entity.getProperty("email");
+
+            Comment comment = new Comment(name, userComment, userEmail);
+            comments.add(comment);
+        }
+        ++i;
+    }
+
+    // Send the JSON as the response
+    Gson gson = new Gson();
+    response.setContentType("application/json;");
+    response.getWriter().println(gson.toJson(comments));
+  }*/
 }
