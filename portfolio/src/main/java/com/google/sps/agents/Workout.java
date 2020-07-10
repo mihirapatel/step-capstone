ac9af3285a3ae771cee397a0dd3c27f96d555c92
Mihira <mihirapatel@berkeley.edu>
2020-07-10 19:39:57 +0000

    Remove API key

--- portfolio/src/main/java/com/google/sps/agents/Workout.java
+++ portfolio/src/main/java/com/google/sps/agents/Workout.java
@@ -6,10 +6,13 @@ import com.google.maps.errors.ApiException;
 import com.google.protobuf.Struct;
 import com.google.protobuf.Value;
 import com.google.sps.data.Video;
+import com.google.sps.utils.WorkoutUtils;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,9 +28,18 @@ public class Workout implements Agent {
   private String output = null;
   private String display = null;
   private String redirect = null;
-  private String workoutType;
-  private String workoutLength;
-  private String youtubeChannel;
+  private String workoutType = "";
+  private String workoutLength = "";
+  private String youtubeChannel = "";
+  private final int numVideos = 5;
+  private String amount = "";
+  private String unit = "";
+  private Video video;
+  private String channelTitle;
+  private String title;
+  private String description;
+  private String thumbnail;
+  private String videoId;
 
   public Workout(String intentName, Map<String, Value> parameters)
       throws IllegalStateException, IOException, ApiException, InterruptedException,
@@ -40,7 +52,6 @@ public class Workout implements Agent {
   public void setParameters(Map<String, Value> parameters)
       throws IllegalStateException, IOException, ApiException, InterruptedException,
           ArrayIndexOutOfBoundsException {
-    log.info(String.valueOf(parameters));
     if (intentName.contains("find")) {
       workoutFind(parameters);
     } else if (intentName.contains("schedule")) {
@@ -63,59 +74,70 @@ public class Workout implements Agent {
     return redirect;
   }
 
-  private void workoutFind(Map<String, Value> parameters)
-      throws IllegalStateException, IOException, ApiException, InterruptedException,
-          ArrayIndexOutOfBoundsException {
-    Struct durationStruct = parameters.get("duration").getStructValue();
-    Map<String, Value> durationMap = durationStruct.getFieldsMap();
-    int amount = (int) durationMap.get("amount").getNumberValue();
-    String unit = durationMap.get("unit").getStringValue();
+  private void workoutFind(Map<String, Value> parameters) throws IOException {
+    log.info(String.valueOf(parameters));
+    String duration = parameters.get("duration").getStringValue();
+    if (!duration.equals("")) {
+      Struct durationStruct = parameters.get("duration").getStructValue();
+      Map<String, Value> durationMap = durationStruct.getFieldsMap();
+      amount = String.valueOf(Math.round(durationMap.get("amount").getNumberValue()));
+      unit = durationMap.get("unit").getStringValue();
+    }
 
-    workoutLength = " " + String.valueOf(amount) + " " + unit;
+    workoutLength = amount + " " + unit;
     workoutType = parameters.get("workout-type").getStringValue();
     youtubeChannel = parameters.get("youtube-channel").getStringValue();
 
     // Set output
-    output = "Here are videos for:" + workoutLength + " " + workoutType + " workout";
+    setWorkoutFindOutput();
+
+    // Set display
+    setWorkoutFindDisplay();
+  }
+
+  private void setWorkoutFindOutput() {
+    output = "Here are videos for: " + workoutLength + " " + workoutType + " workouts";
     if (!youtubeChannel.equals("")) {
       output += " from " + youtubeChannel;
     }
+  }
 
-    // TODO: make API call to WorkoutUtils to get json object of videos
-
-    // Set display
-    Video video1 =
-        new Video(
-            "MadFit",
-            "15 min FULL BODY Fat Burn HIIT Workout (No Equipment)",
-            "An equipment free, beginner friendly, full body, fat burning HIIT style workout to get you nice and sweaty!",
-            "https://i.ytimg.com/vi/dxA21IeBB8o/mqdefault.jpg",
-            "dxA21IeBB8o");
-    Video video2 =
-        new Video(
-            "MadFit",
-            "FULL BODY FAT BURNING HIIT (15 min At Home Workout)",
-            "An intense full body, fat burning, HIIT style, at home workout! No equipment needed, but the use one 2 light dumbbells will increase the intensity.",
-            "https://i.ytimg.com/vi/JMtE0rl21Fg/mqdefault.jpg",
-            "JMtE0rl21Fg");
-    Video video3 =
-        new Video(
-            "MadFit",
-            "FULL BODY FAT BURN HIIT (At Home No Equipment)",
-            "An intense full body, fat burning, HIIT style, at home workout! No equipment needed.",
-            "https://i.ytimg.com/vi/Hc7V7MJCTc8/mqdefault.jpg",
-            "Hc7V7MJCTc8");
+  private void setWorkoutFindDisplay() throws IOException {
+    // Make API call to WorkoutUtils to get json object of videos
+    workoutLength = workoutLength.replaceAll("\\s", "");
+    workoutType = workoutType.replaceAll("\\s", "");
+    youtubeChannel = youtubeChannel.replaceAll("\\s", "");
+    JSONObject json =
+        WorkoutUtils.getJSONObject(workoutLength, workoutType, youtubeChannel, numVideos);
+    JSONArray videos = json.getJSONArray("items");
 
     List<Video> videoList = new ArrayList<>();
-    videoList.add(video1);
-    videoList.add(video2);
-    videoList.add(video3);
+
+    for (int i = 0; i < numVideos; i++) {
+      String videoString = new Gson().toJson(videos.get(i));
+      setVideoParameters(videoString);
+      video = new Video(channelTitle, title, description, thumbnail, videoId);
+      videoList.add(video);
+    }
+
     display = new Gson().toJson(videoList);
   }
 
-  private void workoutSchedule(Map<String, Value> parameters)
-      throws IllegalStateException, IOException, ApiException, InterruptedException,
-          ArrayIndexOutOfBoundsException {
+  private void setVideoParameters(String videoString) {
+    JSONObject videoJSONObject = new JSONObject(videoString).getJSONObject("map");
+    JSONObject id = videoJSONObject.getJSONObject("id").getJSONObject("map");
+    videoId = new Gson().toJson(id.get("videoId"));
+    JSONObject snippet = videoJSONObject.getJSONObject("snippet").getJSONObject("map");
+    description = new Gson().toJson(snippet.get("description"));
+    title = new Gson().toJson(snippet.get("title"));
+    channelTitle = new Gson().toJson(snippet.get("channelTitle"));
+    JSONObject thumbnailJSONObject =
+        snippet.getJSONObject("thumbnails").getJSONObject("map").getJSONObject("medium");
+    JSONObject thumbnailURL = thumbnailJSONObject.getJSONObject("map");
+    thumbnail = new Gson().toJson(thumbnailURL.get("url"));
+  }
+
+  private void workoutSchedule(Map<String, Value> parameters) {
     return;
   }
 }
---
+++ portfolio/src/main/java/com/google/sps/utils/WorkoutUtils.java
@@ -0,0 +1,49 @@
+package com.google.sps.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class WorkoutUtils {
+
+  private static String readAll(Reader rd) throws IOException {
+    StringBuilder sb = new StringBuilder();
+    int cp;
+    while ((cp = rd.read()) != -1) {
+      sb.append((char) cp);
+    }
+    return sb.toString();
+  }
+
+  public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+    InputStream is = new URL(url).openStream();
+    try {
+      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+      String jsonText = readAll(rd);
+      JSONObject json = new JSONObject(jsonText);
+      return json;
+    } finally {
+      is.close();
+    }
+  }
+
+  public static JSONObject getJSONObject(
+      String workoutLength, String workoutType, String youtubeChannel, int numVideos)
+      throws IOException, JSONException {
+    String URL = "https://www.googleapis.com/youtube/v3/search?part=snippet";
+    String maxResults = "maxResults=" + String.valueOf(numVideos);
+    String q = "q=" + String.join("+", workoutLength, workoutType, youtubeChannel);
+    String type = "type=video";
+    String key = "key=API_KEY_HERE";
+    URL = String.join("&", URL, maxResults, q, type, key);
+    System.out.println(URL);
+    JSONObject json = readJsonFromUrl(URL);
+    return json;
+  }
+}
