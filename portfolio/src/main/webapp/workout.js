4c51ff76585f057454d0f180f07cdcaa1e6413a9
Mihira <mihirapatel@berkeley.edu>
2020-07-10 23:16:09 +0000

    video thumbnails now show up

---
+++ portfolio/src/main/webapp/workout.js
@@ -0,0 +1,35 @@
+function workoutVideos(videoQuery) {
+  var videos = JSON.parse(videoQuery);
+  let workoutDiv = createVideoDivs(videos);
+  return workoutDiv;
+}
+
+function createVideoDivs(videos) {
+  
+  workoutDiv = document.createElement('div');
+  workoutDiv.classList.add('media-display');
+
+  videosDiv = document.createElement('div');
+  videosDiv.id = 'videos';
+  workoutDiv.append(videosDiv);
+
+  for (var i = 0; i < videos.length; i++) {
+    var video = videos[i];
+    var channelTitle = video.channelTitle;
+    var title = video.title;
+    var description = video.description;
+
+    var videoLink = document.createElement('a');
+    var thumbnailImage = document.createElement('img');
+    thumbnailImage.src = video.thumbnail.replace(/"/g, '');
+    thumbnailImage.setAttribute("width", "320");
+    thumbnailImage.setAttribute("height", "180");
+    videoLink.appendChild(thumbnailImage);
+    videoLink.title = title
+    videoLink.href = video.videoURL;
+    videoLink.target = "_blank";
+    videosDiv.appendChild(videoLink);
+  }
+  
+  return workoutDiv;
+}
