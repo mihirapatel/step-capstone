1af47302ffcb261c13a581baf1f30f6732150fa2
Abby Mapes <acm103@duke.edu>
2020-07-15 00:13:04 +0000

    Add sessionID handling to get to BookAgent

--- portfolio/src/main/java/com/google/sps/utils/AgentUtils.java
+++ portfolio/src/main/java/com/google/sps/utils/AgentUtils.java
@@ -37,12 +37,15 @@ public class AgentUtils {
    * @param userServiceInput UserService instance to access userID and other user info if necessary.
    * @param datastoreInput DatastoreService instance used to access past comments from the user's
    *     database if necessary.
+   * @param sessionID unique sessionID for current session of AIssistant running used to store
+   *     BookQuery and book results for users who are not logged in
    */
   public static Output getOutput(
       DialogFlowClient queryResult,
       String languageCode,
       UserService userServiceInput,
-      DatastoreService datastoreInput) {
+      DatastoreService datastoreInput,
+      String sessionID) {
     String fulfillment = null;
     String display = null;
     String redirect = null;
@@ -69,7 +72,7 @@ public class AgentUtils {
     // Set fulfillment if parameters are present, upon any exceptions return default
     if (allParamsPresent) {
       try {
-        object = createAgent(agentName, intentName, detectedInput, parameterMap);
+        object = createAgent(agentName, intentName, detectedInput, parameterMap, sessionID);
         fulfillment = object.getOutput();
         fulfillment = fulfillment == null ? queryResult.getFulfillmentText() : fulfillment;
         display = object.getDisplay();
@@ -101,12 +104,16 @@ public class AgentUtils {
   }
 
   private static Agent createAgent(
-      String agentName, String intentName, String queryText, Map<String, Value> parameterMap)
+      String agentName,
+      String intentName,
+      String queryText,
+      Map<String, Value> parameterMap,
+      String sessionID)
       throws IllegalStateException, IOException, ApiException, InterruptedException,
           ArrayIndexOutOfBoundsException {
     switch (agentName) {
       case "books":
-        return new BooksAgent(intentName, queryText, parameterMap);
+        return new BooksAgent(intentName, queryText, parameterMap, sessionID);
       case "calculator":
         return new Tip(intentName, parameterMap);
       case "currency":
@@ -143,7 +150,7 @@ public class AgentUtils {
     return intentList[0];
   }
 
-  private static String getIntentName(String detectedIntent) {
+  public static String getIntentName(String detectedIntent) {
     String[] intentList = detectedIntent.split("\\.", 2);
     String intentName = detectedIntent;
     if (intentList.length > 1) {
