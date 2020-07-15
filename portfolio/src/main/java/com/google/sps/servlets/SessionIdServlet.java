bb9f1fe8d0dc12c17263d30ecb6327679d195217
Abby Mapes <acm103@duke.edu>
2020-07-15 00:13:56 +0000

    Add ID assigning and deleting after sessions

--- portfolio/src/main/java/com/google/sps/servlets/SessionIdServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/SessionIdServlet.java
@@ -7,16 +7,16 @@ import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+import com.google.sps.utils.BooksMemoryUtils;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Retrieves a unique sessionID for each user (logged in or not) in order to keep track of their
- * stored BookQuery information
+ * Retrieves sessionIDs for each user (logged in or not) in order to keep track of their stored Book
+ * information and deletes stored session Book Information when session is over
  */
 @WebServlet("/id")
 public class SessionIdServlet extends HttpServlet {
@@ -24,19 +24,23 @@ public class SessionIdServlet extends HttpServlet {
   UserService userService = createUserService();
   DatastoreService datastore = createDatastore();
 
+  /**
+   * Retrieves a unique sessionID for each user in order to keep track of their stored Book
+   * information
+   *
+   * <p>If the user is logged in, their sessionID is their userID. Otherwise, a unique guest session
+   * ID is generated for their session ID
+   */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html");
-    PrintWriter out = response.getWriter();
     String id;
-    String loginUrl = userService.createLoginURL("/index.html");
-    String authText;
 
     if (userService.isUserLoggedIn()) {
       id = userService.getCurrentUser().getUserId();
     } else {
       int prevId = 0;
-      Query query = new Query("GuestID");
+      Query query = new Query("SessionID");
       PreparedQuery results = datastore.prepare(query);
 
       int numIds = 0;
@@ -46,13 +50,19 @@ public class SessionIdServlet extends HttpServlet {
         datastore.delete(entity.getKey());
         ++numIds;
       }
-      Entity entity = new Entity("GuestID");
+      Entity entity = new Entity("SessionID");
       entity.setProperty("idNum", prevId + 1);
       datastore.put(entity);
       id = "guest" + Integer.toString(prevId + 1);
     }
-
     response.getWriter().write(id);
+  }
+
+  /** Deletes all stored Entity in Datastore that match the "session-id" parameter */
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String sessionID = request.getParameter("session-id");
+    BooksMemoryUtils.deleteAllStoredBookInformation(sessionID);
   }
 
   protected UserService createUserService() {
