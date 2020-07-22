d1903a94573e0d990ceac9abb2f745f2c8a36489
Mihira <mihirapatel@berkeley.edu>
2020-07-22 23:00:39 +0000

    Create WorkoutPlan object for cleaner code

---
+++ portfolio/src/main/java/com/google/sps/data/WorkoutPlan.java
@@ -0,0 +1,29 @@
+package com.google.sps.data;
+
+import com.google.gson.Gson;
+import java.io.Serializable;
+import java.util.List;
+
+/** WorkoutPlan class that stores userId and corresponding workout plan */
+public final class WorkoutPlan implements Serializable {
+
+  private String userId;
+  private List<List<YouTubeVideo>> workoutPlanPlaylist;
+
+  public WorkoutPlan(String userId, List<List<YouTubeVideo>> workoutPlanPlaylist) {
+    this.userId = userId;
+    this.workoutPlanPlaylist = workoutPlanPlaylist;
+  }
+
+  public String getUserId() {
+    return this.userId;
+  }
+
+  public List<List<YouTubeVideo>> getWorkoutPlanPlaylist() {
+    return this.workoutPlanPlaylist;
+  }
+
+  public String toString() {
+    return new Gson().toJson(this);
+  }
+}
