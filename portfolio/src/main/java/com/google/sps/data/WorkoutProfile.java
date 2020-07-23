69a4518dc8b23e6c75a9b0ddebb08095d4e9c137
Mihira <mihirapatel@berkeley.edu>
2020-07-23 17:36:33 +0000

    Added WorkoutProfile class to store user information

---
+++ portfolio/src/main/java/com/google/sps/data/WorkoutProfile.java
@@ -0,0 +1,46 @@
+package com.google.sps.data;
+
+import com.google.gson.Gson;
+
+/**
+ * WorkoutProfile that has information about everything related to the Workout Agent User
+ * information: Name, email Workout information: Workout plans, workout videos completed, minutes
+ * worked out
+ */
+public class WorkoutProfile {
+
+  private String userId;
+  private String userName = ""; // TODO
+  private String userEmail = ""; // TODO
+  private ArrayList<WorkoutPlan> workoutPlans;
+  private ArrayList<YouTubeVideo> vidoesWatched = null; // TODO
+
+  public WorkoutProfile(String userId, ArrayList<WorkoutPlan> workoutPlans) {
+    this.userId = userId;
+    this.workoutPlans = workoutPlans;
+  }
+
+  public String getUserId() {
+    return this.userId;
+  }
+
+  public String getUserName() {
+    return this.userName;
+  }
+
+  public String getUserEmail() {
+    return this.userEmail;
+  }
+
+  public ArrayList<WorkoutPlan> getWorkouts() {
+    return this.workoutPlans;
+  }
+
+  public ArrayList<YouTubeVideo> getVideosWatched() {
+    return this.vidoesWatched;
+  }
+
+  public String toString() {
+    return new Gson().toJson(this);
+  }
+}
