b52e64ce00d5a0b504f96d55e026b797d40cdaf5
Abby Mapes <acm103@duke.edu>
2020-07-23 00:33:11 +0000

    Add frontend for delete and add intents

--- portfolio/src/main/webapp/book.js
+++ portfolio/src/main/webapp/book.js
@@ -79,7 +79,7 @@ function createBookShelfRow(bookshelfName) {
   bookshelfButton.className = "bookshelf-button";
   bookshelfButton.insertAdjacentHTML('afterbegin', bookshelfName);
   bookshelfButton.addEventListener("click", function () {
-    getBookshelfInformation('books.library', bookshelfName);
+    goToBookshelf('books.library', bookshelfName);
   });
   bookshelfColumn.appendChild(bookshelfButton);
   bookRow.appendChild(bookshelfColumn);
@@ -269,14 +269,30 @@ function createLinkColumn(book, queryID) {
   }
   addLogo = '<img class = "redirect-logo" alt="Redirect" src= "images/redirect.png" >';
   paragraph.insertAdjacentHTML('beforeend', addLogo);
-
+  
+  // TODO: only show up if user is logged in 
   var libraryLink = document.createElement('a');
-  libraryLink.className = "book-link";
+  libraryLink.className = "add-link";
   libraryLink.insertAdjacentHTML('afterbegin', "Add to My Library");
   libraryLink.addEventListener("click", function () {
     getBookshelfNamesFromButton('books.add', book.order, queryID);
   });
   paragraph.appendChild(libraryLink);
+  
+  if (queryID.includes("-shelf")) {
+    deleteLogo = '<img class = "redirect-logo" alt="Redirect" src= "images/redirect.png" >';
+    paragraph.insertAdjacentHTML('beforeend', "<br>" + deleteLogo);
+
+    var deleteLink = document.createElement('a');
+    deleteLink.className = "delete-link";
+    deleteLink.insertAdjacentHTML('afterbegin', "Remove from shelf");
+    deleteLink.addEventListener("click", function () {
+      editBookshelf('books.delete', "", book.order, queryID);
+    });
+    paragraph.appendChild(deleteLink);
+  }
+  
+
   linkColumn.appendChild(paragraph);
   return linkColumn;
 }
@@ -295,22 +311,20 @@ function updateBookScroll(queryID) {
 }
 
 /**
- * Displays book description and fulfilment text from the stream
- * from Dialogflow
+ * Displays description of the book added to the user's bookshelf and 
+ * fulfilment text from the stream from Dialogflow
  *
  * @param stream output object stream from Dialogflow
+ * @param bookshelfName name of bookshelf the book was added to
  */
-function displayBookInfo(stream) {
+function displayBookAdded(stream, bookshelfName) {
   var outputAsJson = JSON.parse(stream);
-
   if (outputAsJson.display) {
     clearPreviousDisplay(outputAsJson.redirect);
-
     placeBooksUserInput(outputAsJson.userInput, "convo-container", outputAsJson.redirect);
     placeBooksFulfillment(outputAsJson.fulfillmentText, outputAsJson.redirect);
-    infoContainer = createBookInfoContainer(outputAsJson.display, outputAsJson.intent, outputAsJson.redirect);
+    infoContainer = createBookInfoContainer(outputAsJson.display, outputAsJson.intent, outputAsJson.redirect, bookshelfName);
     placeBookDisplay(infoContainer, "convo-container", outputAsJson.redirect);
-    
     if (outputAsJson.intent.includes("preview")) {
       loadPreview(outputAsJson.display);
     }
@@ -328,9 +342,10 @@ function displayBookInfo(stream) {
  * @param bookResult JSON Book returned from Dialogflow
  * @param intent String specifying intent 
  * @param queryID appropriate queryID for results in this element
+ * @param intent String specifying bookshelf current book is being added to 
  * @return infoDiv div element containing information table
  */
-function createBookInfoContainer(bookResult, intent, queryID){
+function createBookInfoContainer(bookResult, intent, queryID, bookshelfName){
   var book = JSON.parse(bookResult);
 
   infoDiv = document.createElement("div"); 
@@ -340,7 +355,22 @@ function createBookInfoContainer(bookResult, intent, queryID){
 
   infoTable.appendChild(createInfoRow(book, intent));
   infoTable.appendChild(createBookRow(book, queryID));
-  infoTable.appendChild(createInfoFooter(queryID));
+  const footerRow = createInfoFooter(queryID);
+  if (bookshelfName) {
+    footerRow.insertAdjacentHTML("beforeend", "<td></td>");
+    const bookshelfCol = document.createElement('td');
+    bookshelfCol.className = ("more-column");
+
+    var shelfButton = document.createElement("button");
+    shelfButton.className = "book-button-" + queryID;
+    shelfButton.insertAdjacentHTML('afterbegin', "Go to " + bookshelfName);
+    shelfButton.addEventListener("click", function () {
+        goToBookshelf('books.library', bookshelfName);
+    });
+    bookshelfCol.appendChild(shelfButton);
+    footerRow.appendChild(bookshelfCol);
+  }
+  infoTable.appendChild(footerRow);
   infoDiv.appendChild(infoTable);
 
   return infoDiv;
@@ -383,6 +413,7 @@ function createInfoRow(book, intent){
 function createInfoFooter(queryID){
   const footerRow = document.createElement('tr');
   footerRow.className = "book-row";
+  const footerCol = document.createElement('td');
   
   var backButton = document.createElement("button");
   backButton.className = "book-button-" + queryID;
@@ -390,8 +421,8 @@ function createInfoFooter(queryID){
   backButton.addEventListener("click", function () {
     getBooksFromButton('books.results', queryID);
   });
-
-  footerRow.appendChild(backButton);
+  footerCol.appendChild(backButton);
+  footerRow.appendChild(footerCol);
   return footerRow;
 }
 
@@ -440,6 +471,7 @@ function displayBookshelvesToAdd(stream, bookIndex, queryID) {
   placeBooksFulfillment(outputAsJson.fulfillmentText, queryID);
   bookShelfAddContainer = createBookshelfAddContainer(outputAsJson.display, bookIndex, queryID);
   placeBookDisplay(bookShelfAddContainer, "convo-container", queryID);
+  outputAudio(stream);
 }
 
 /**
@@ -488,36 +520,11 @@ function createBookShelfAddRow(bookshelfName, bookIndex, queryID) {
   bookshelfButton.className = "bookshelf-button";
   bookshelfButton.insertAdjacentHTML('afterbegin', bookshelfName);
   bookshelfButton.addEventListener("click", function () {
-    addToBookshelf('books.add', bookshelfName, bookIndex, queryID);
+    editBookshelf('books.add', bookshelfName, bookIndex, queryID);
   });
   bookshelfColumn.appendChild(bookshelfButton);
   bookRow.appendChild(bookshelfColumn);
   return bookRow;
-}
-
-/**
- * Displays book results without user input text when one of the  
- * buttons in the display has been pressed, triggering a book results
- * display 
- *
- * @param stream output object stream from Dialogflow
- */
-function displayBooksFromButton(stream) {
-  var outputAsJson = JSON.parse(stream);
-  if (outputAsJson.intent.includes("books.more") ||
-        outputAsJson.intent.includes("books.previous") ||
-        outputAsJson.intent.includes("books.results") || 
-        outputAsJson.intent.includes("books.library")){
-    clearPreviousDisplay(outputAsJson.redirect);
-
-    placeBooksUserInput(outputAsJson.userInput, "convo-container", outputAsJson.redirect);
-    placeBooksFulfillment(outputAsJson.fulfillmentText, outputAsJson.redirect);
-    bookContainer = createBookContainer(outputAsJson.display, outputAsJson.redirect);
-    placeBookDisplay(bookContainer, "convo-container", outputAsJson.redirect);
-  } else {
-    placeFulfillmentResponse(outputAsJson.fulfillmentText);
-  }
-  outputAudio(stream);
 }
 
 /**
