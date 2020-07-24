0223b8849c444cf8476452aef465d780235d7b54
Mihira <mihirapatel@berkeley.edu>
2020-07-24 04:51:39 +0000

    Workout profile servlet commented out

--- portfolio/src/main/java/com/google/sps/servlets/WorkoutProfileServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/WorkoutProfileServlet.java
@@ -24,5 +24,12 @@ public class WorkoutProfileServlet extends HttpServlet {
     response.setContentType("application/json");
     String userId = userService.getCurrentUser().getUserId();
     // TODO
+
+    // ArrayList<WorkoutPlan> workoutPlans =
+    //    WorkoutProfileUtils.getWorkoutPlansList(userId, datastore);
+    // WorkoutProfile workoutProfile = new WorkoutProfile(userId, workoutPlans);
+
+    // String json = new Gson().toJson(workoutProfile);
+    // response.getWriter().write(json);
   }
 }
